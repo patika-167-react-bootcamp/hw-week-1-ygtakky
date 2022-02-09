47be1134afa62bc765263d7212ea7925aefd2c97
Yiğit Akkaya <ygtakky@gmail.com>
2022-02-09 22:05:34 +0300

    [MOD] Hw 1- fixed unneccesary looping

--- hw-1.js
+++ hw-1.js
@@ -31,32 +31,42 @@ Array.prototype.move = function (fileId, folderId) {
   const fileFolders = folders.filter(function (element) {
     return element.files;
   });
-  // Give copied file a default value of -1 for not finding a file
+  // Give moved file a default value of -1 for not finding a file
   let movedFile = -1;
-  fileFolders.map(function (element) {
-    element.files.map(function (file, i) {
-      // If file is found delete file with its index and return the deleted file
-      if (file.id === fileId) {
-        movedFile = element.files.splice(i, 1);
+  fileFoldersLength = fileFolders.length;
+  // Loop through folder array and file arrays and stop when required file id is found
+  while (fileFoldersLength--) {
+    filesLength = fileFolders[fileFoldersLength].files.length;
+    while (filesLength--) {
+      if (fileFolders[fileFoldersLength].files[filesLength].id === fileId) {
+        movedFile = fileFolders[fileFoldersLength].files.splice(filesLength, 1);
+        break;
       }
-    });
-  });
+    }
+    if (movedFile !== -1) {
+      break;
+    }
+  }
+
   // If file with given file Id do not exist return -1
   if (movedFile === -1) {
     console.log("No file with that id!");
     return -1;
   }
   // Get the files in the target folder
-  this.map(function (element) {
-    if (element.id === folderId) {
-      if (element.files) {
-        element.files.push(movedFile[0]);
+  arrayLength = this.length
+  while (arrayLength--)
+  {
+    if (this[arrayLength].id === folderId)
+    {
+      if (this[arrayLength].files) {
+        this[arrayLength].files.push(movedFile[0]);
       } else {
-        element.files = [];
-        element.files.push(movedFile[0]);
+        this[arrayLength].files = [];
+        this[arrayLength].files.push(movedFile[0]);
       }
     }
-  });
+  }
   return movedFile[0];
 };
 
@@ -72,30 +82,42 @@ Array.prototype.copy = function (fileId, folderId) {
   });
   // Give copied file a default value of -1 for not finding a file
   let copiedFile = -1;
-  fileFolders.map(function (element) {
-    element.files.map(function (file, i) {
-      // If file is found delete file with its index and return the deleted file
-      if (file.id === fileId) {
-        copiedFile = element.files.slice(i, i + 1);
+  fileFoldersLength = fileFolders.length;
+  // Loop through folder array and file arrays and stop when required file id is found
+  while (fileFoldersLength--) {
+    filesLength = fileFolders[fileFoldersLength].files.length;
+    while (filesLength--) {
+      if (fileFolders[fileFoldersLength].files[filesLength].id === fileId) {
+        copiedFile = fileFolders[fileFoldersLength].files.slice(
+          filesLength,
+          filesLength + 1
+        );
+        break;
       }
-    });
-  });
+    }
+    if (copiedFile !== -1) {
+      break;
+    }
+  }
   // If file with given file Id do not exist return -1
   if (copiedFile === -1) {
     console.log("No file with that id!");
     return -1;
   }
   // Get the files in the target folder
-  this.map(function (element) {
-    if (element.id === folderId) {
-      if (element.files) {
-        element.files.push(copiedFile[0]);
+  arrayLength = this.length
+  while (arrayLength--)
+  {
+    if (this[arrayLength].id === folderId)
+    {
+      if (this[arrayLength].files) {
+        this[arrayLength].files.push(copiedFile[0]);
       } else {
-        element.files = [];
-        element.files.push(copiedFile[0]);
+        this[arrayLength].files = [];
+        this[arrayLength].files.push(copiedFile[0]);
       }
     }
-  });
+  }
   return copiedFile[0];
 };
 
@@ -112,14 +134,20 @@ Array.prototype.remove = function (fileId) {
   });
   // Give deleted file a default value of -1 for not finding a file
   let deletedFile = -1;
-  fileFolders.map(function (element) {
-    element.files.map(function (file, i) {
-      // If file is found delete file with its index and return the deleted file
-      if (file.id === fileId) {
-        deletedFile = element.files.splice(i, 1);
+  fileFoldersLength = fileFolders.length;
+  // Loop through folder array and file arrays and stop when required file id is found
+  while (fileFoldersLength--) {
+    filesLength = fileFolders[fileFoldersLength].files.length;
+    while (filesLength--) {
+      if (fileFolders[fileFoldersLength].files[filesLength].id === fileId) {
+        deletedFile = fileFolders[fileFoldersLength].files.splice(filesLength, 1);
+        break;
       }
-    });
-  });
+    }
+    if (deletedFile !== -1) {
+      break;
+    }
+  }
   return deletedFile;
 };
 
@@ -155,13 +183,19 @@ Array.prototype.parentFolderOf = function (fileId) {
   });
   // Give a default index of -1 for not finding the file
   let index = -1;
-  fileFolders.map(function (element) {
-    element.files.map(function (file) {
-      if (file.id === fileId) {
-        index = element.id;
+  fileFoldersLength = fileFolders.length;
+  // Loop through folder array and file arrays and stop when required file id is found
+  while (fileFoldersLength--) {
+    filesLength = fileFolders[fileFoldersLength].files.length;
+    while (filesLength--) {
+      if (fileFolders[fileFoldersLength].files[filesLength].id === fileId) {
+        index = fileFolders[fileFoldersLength].id;
+        break;
       }
-      return;
-    });
-  });
+    }
+    if (index !== -1) {
+      break;
+    }
+  }
   return index;
 };
